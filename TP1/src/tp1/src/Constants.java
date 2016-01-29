11401ae0bf363fa9291c60d047a8a9dd8b08d257
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-01-29 11:12:21 +0100

    Added return codes

--- TP1/src/tp1/src/Constants.java
+++ TP1/src/tp1/src/Constants.java
@@ -9,4 +9,14 @@ public class Constants {
 	public static final String CMD_STOR = "STOR";
 	public static final String CMD_LIST = "LIST";
 	public static final String CMD_QUIT = "QUIT";
+	
+	// Return codes
+	public static final int CODE_CONNECTION_SUCC = 220;
+	public static final int CODE_TRANSFER_SUCC = 226;
+	public static final int CODE_DISCONNECTION = 221;
+	public static final int CODE_AUTH_SUCC = 230;
+	public static final int CODE_WAITING_PASS = 331;
+	public static final int CODE_AUTH_FAILED = 430;
+	public static final int CODE_INVALID_CMD = 500;
+	public static final int CODE_INVALID_PARAM = 501;
 }
